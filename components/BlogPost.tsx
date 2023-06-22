4c8ea0be60f19b007410a9e2890039dc23cd8e12
Michael <michael.sage@talktalk.net>
2023-06-23 00:05:46 +0100

    Update BlogPost.tsx

--- components/BlogPost.tsx
+++ components/BlogPost.tsx
@@ -60,69 +60,6 @@ const BlogPostImage = styled.img`
   max-height: 250px;
 `
 
-// const BlogPostContentWIP = styled.div` // TODO make table more responsive by implementing this and Table.tsx
-//   width: 100%;
-//   table {
-//     border-collapse: collapse;
-//     border-spacing: 0;
-//     width: 100%;
-//     margin-bottom: 1rem;
-//     border: 1px solid #dee2e6;
-//     border-radius: 0.25rem;
-//     overflow-x: auto;
-
-//     thead {
-//       font-weight: 700;
-//       background-color: #f5f5f5;
-//     }
-//     tr {
-//       padding: 5px;
-//       border: 1px solid #dee2e6;
-//       td:first-child {
-//         font-weight: 700;
-//         background-color: #f5f5f5;
-//       }
-//     }
-//     td {
-//       padding: 5px;
-//     }
-//   }
-
-//   @media screen and (max-width: 600px) {
-
-//     table {
-//       border: 0;
-//     }
-
-//     // table thead {
-//     //   display: none;
-//     // }
-
-//     table tr {
-//       margin-bottom: 10px;
-//       display: block;
-//       border-bottom: 2px solid #ddd;
-//     }
-
-//     table td {
-//       display: block;
-//       text-align: right;
-//       font-size: 13px;
-//       border-bottom: 1px dotted #ccc;
-//     }
-
-//     table td:last-child {
-//       border-bottom: 0;
-//     }
-
-//     table td:before {
-//       content: attr(data-label);
-//       float: left;
-//       font-weight: bold;
-//     }
-//   }
-// `
-
 const BlogPostContent = styled.div`
   width: 100%;
 
