579640c1a35e04458e3b601c5ae1f54a5e1b95b7
Michael <michael.sage@talktalk.net>
2023-06-28 19:26:57 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Head from 'next/head';
 import { Footer } from '../components/Footer';
 import { globals } from '../globals';
 import { Header } from '../components/Header';
